6b812ce0e383ec1d82bbb05a2fccc22684ed3051
Ankit <40174133+Ap1297@users.noreply.github.com>
2024-08-08 23:54:19 +0530

    username and password from db

--- src/main/java/com/thunderCoder/model/config/SecurityConfig.java
+++ src/main/java/com/thunderCoder/model/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.thunderCoder.model.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.thunderCoder.service.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	MyUserDetailsService userDetailsService;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +30,25 @@ public class SecurityConfig {
 		.httpBasic(Customizer.withDefaults()) // added for postman for rest api access
 		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build(); // for working with postman for stateless session
 		
-		
 	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setUserDetailsService(userDetailsService);
+		return provider;
+	}
+	
+	/*@Bean
+	public UserDetailsService userDetailsService() {
+		
+		 * UserDetails user1 = User .withDefaultPasswordEncoder() // password without
+		 * encryption .username("ankit") .password("ankit") .roles("USER") .build();
+		 * 
+		 * UserDetails user2 = User .withDefaultPasswordEncoder() // password without
+		 * encryption .username("piyu") .password("piyu") .roles("ADMIN") .build();
+		 * return new InMemoryUserDetailsManager(user1,user2);
+		 // For hardcoding stuff
+	} */
 }
---
+++ src/main/java/com/thunderCoder/repo/UserRepo.java
@@ -0,0 +1,15 @@
+package com.thunderCoder.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.thunderCoder.model.Users;
+
+@Repository
+@Component
+public interface UserRepo extends JpaRepository<Users, Integer>{
+
+	Users findByUsername(String username);
+
+}
